34a314d184e075d2651348f3365a583dfc7af30b
antoniodavidperez <48296472+antoniodavidperez@users.noreply.github.com>
2020-02-24 18:57:07 +0100

    Add files via upload

---
+++ Tienda/Tienda/__manifest__.py
@@ -0,0 +1,7 @@
+{
+    'name': 'Tienda',
+    'description': 'Registro de ventas.',
+    'author': 'Antonio David Pérez Gutiérrez',
+    'application': True,
+    'data': ['./views/Productos.xml', './views/LineasFacturas.xml', './views/Facturas.xml']
+}
